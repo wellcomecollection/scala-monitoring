5e0d82b14f56ca264a4ac0d995da9d7ddd9e9562
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-11 17:16:31 +0000

    Add the mock metrics sender

--- monitoring/src/test/scala/uk/ac/wellcome/monitoring/fixtures/MetricsSenderFixture.scala
+++ monitoring/src/test/scala/uk/ac/wellcome/monitoring/fixtures/MetricsSenderFixture.scala
@@ -1,15 +1,21 @@
 package uk.ac.wellcome.monitoring.fixtures
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch
+import org.mockito.Matchers.anyString
+import org.mockito.Mockito.when
+import org.mockito.invocation.InvocationOnMock
+import org.scalatest.mockito.MockitoSugar
 import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.fixtures._
 import uk.ac.wellcome.monitoring.{MetricsConfig, MetricsSender}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 trait MetricsSenderFixture
     extends CloudWatch
-    with Akka {
+    with Akka
+    with MockitoSugar {
 
   def withMetricsSender[R](
     cloudWatchClient: AmazonCloudWatch = cloudWatchClient)(
@@ -27,4 +33,16 @@ trait MetricsSenderFixture
         testWith(metricsSender)
       }
     }
+
+  def withMockMetricsSender[R](testWith: TestWith[MetricsSender, R]): R = {
+    val metricsSender = mock[MetricsSender]
+
+    when(
+      metricsSender.incrementCount(anyString())
+    ).thenAnswer((invocation: InvocationOnMock) => {
+      Future.successful(())
+    })
+
+    testWith(metricsSender)
+  }
 }
