5ad12e4ebdfc29f716a99b8fe2a46ca22db19dc2
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-11 15:29:34 +0000

    Remove most of the tests related to count()

--- monitoring/src/test/scala/uk/ac/wellcome/monitoring/MetricsSenderTest.scala
+++ monitoring/src/test/scala/uk/ac/wellcome/monitoring/MetricsSenderTest.scala
@@ -48,94 +48,33 @@ class MetricsSenderTest
     }
   }
 
+  it("groups 20 MetricDatum into one PutMetricDataRequest") {
+    withMonitoringActorSystem { actorSystem =>
+      val amazonCloudWatch = mock[AmazonCloudWatch]
+      withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
+        val capture = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
+
+        val metricName = createMetricName
+
+        val futures =
+          (1 to 40).map { _ => metricsSender.incrementCount(metricName) }
+
+        whenReady(Future.sequence(futures)) { _ =>
+          eventually {
+            verify(amazonCloudWatch, times(2)).putMetricData(capture.capture())
+
+            val putMetricDataRequests = capture.getAllValues
+            putMetricDataRequests should have size 2
+
+            putMetricDataRequests.asScala.head.getMetricData should have size 20
+            putMetricDataRequests.asScala.tail.head.getMetricData should have size 20
+          }
+        }
+      }
+    }
+  }
+
   describe("count") {
-    it("counts a successful future") {
-      withMonitoringActorSystem { actorSystem =>
-        val amazonCloudWatch = mock[AmazonCloudWatch]
-        withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
-          val expectedResult = "foo"
-          val f = Future {
-            expectedResult
-          }
-          val metricName = createMetricName
-
-          metricsSender.count(metricName, f)
-
-          whenReady(f) { result =>
-            result shouldBe expectedResult
-            assertSingleDataPoint(
-              amazonCloudWatch, metricName = s"${metricName}_success")
-          }
-        }
-      }
-    }
-
-    it("counts a failed future") {
-      withMonitoringActorSystem { actorSystem =>
-        val amazonCloudWatch = mock[AmazonCloudWatch]
-        withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
-          val f = Future {
-            throw new RuntimeException()
-          }
-          val metricName = createMetricName
-
-          metricsSender.count(metricName, f)
-
-          whenReady(f.failed) { _ =>
-            assertSingleDataPoint(
-              amazonCloudWatch, metricName = s"${metricName}_failure")
-          }
-        }
-      }
-    }
-
-    it("counts a recognised failure") {
-      withMonitoringActorSystem { actorSystem =>
-        val amazonCloudWatch = mock[AmazonCloudWatch]
-        withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
-          val f = Future {
-            throw new RecognisedFailureException(
-              new RuntimeException("AAARGH!")
-            )
-          }
-          val metricName = createMetricName
-
-          metricsSender.count(metricName, f)
-
-          whenReady(f.failed) { _ =>
-            assertSingleDataPoint(
-              amazonCloudWatch, metricName = s"${metricName}_recognisedFailure")
-          }
-        }
-      }
-    }
-
-    it("groups 20 MetricDatum into one PutMetricDataRequest") {
-      withMonitoringActorSystem { actorSystem =>
-        val amazonCloudWatch = mock[AmazonCloudWatch]
-        withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
-          val capture = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
-
-          val f = Future.successful(())
-          val metricName = createMetricName
-
-          val futures =
-            (1 to 40).map(i => metricsSender.count(metricName, f))
-
-          whenReady(Future.sequence(futures)) { _ =>
-            eventually {
-              verify(amazonCloudWatch, times(2)).putMetricData(capture.capture())
-
-              val putMetricDataRequests = capture.getAllValues
-              putMetricDataRequests should have size 2
-
-              putMetricDataRequests.asScala.head.getMetricData should have size 20
-              putMetricDataRequests.asScala.tail.head.getMetricData should have size 20
-            }
-          }
-        }
-      }
-    }
 
     it("sends a success metric from countSuccess") {
       val metricName = createMetricName
@@ -170,42 +109,43 @@ class MetricsSenderTest
       )
     }
 
-    it("takes at least one second to make 150 PutMetricData requests") {
-      withMonitoringActorSystem { actorSystem =>
-        val amazonCloudWatch = mock[AmazonCloudWatch]
-        withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
-          val capture = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
 
-          val f = Future.successful(())
-          val metricName = createMetricName
+  }
 
-          val expectedDuration = (1 second).toMillis
-          val startTime = Instant.now
+  it("takes at least one second to make 150 PutMetricData requests") {
+    withMonitoringActorSystem { actorSystem =>
+      val amazonCloudWatch = mock[AmazonCloudWatch]
+      withMetricsSender(actorSystem, amazonCloudWatch) { metricsSender =>
+        val capture = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
 
-          // Each PutMetricRequest is made of 20 MetricDatum so we need
-          // 20 * 150 = 3000 calls to incrementCount to get 150 PutMetricData calls
-          val futures =
-            (1 to 3000).map(i => metricsSender.count(s"${i}_$metricName", f))
+        val metricName = createMetricName
 
-          val promisedInstant = Promise[Instant]
+        val expectedDuration = (1 second).toMillis
+        val startTime = Instant.now
 
-          whenReady(Future.sequence(futures)) { _ =>
-            eventually {
-              verify(amazonCloudWatch, times(150))
-                .putMetricData(capture.capture())
+        // Each PutMetricRequest is made of 20 MetricDatum so we need
+        // 20 * 150 = 3000 calls to incrementCount to get 150 PutMetricData calls
+        val futures =
+        (1 to 3000).map { i => metricsSender.incrementCount(s"${i}_$metricName") }
 
-              val putMetricDataRequests = capture.getAllValues
+        val promisedInstant = Promise[Instant]
 
-              putMetricDataRequests should have size 150
+        whenReady(Future.sequence(futures)) { _ =>
+          eventually {
+            verify(amazonCloudWatch, times(150))
+              .putMetricData(capture.capture())
 
-              promisedInstant.success(Instant.now())
-            }
+            val putMetricDataRequests = capture.getAllValues
+
+            putMetricDataRequests should have size 150
+
+            promisedInstant.success(Instant.now())
           }
+        }
 
-          whenReady(promisedInstant.future) { endTime =>
-            val gap: Long = ChronoUnit.MILLIS.between(startTime, endTime)
-            gap shouldBe >(expectedDuration)
-          }
+        whenReady(promisedInstant.future) { endTime =>
+          val gap: Long = ChronoUnit.MILLIS.between(startTime, endTime)
+          gap shouldBe >(expectedDuration)
         }
       }
     }
