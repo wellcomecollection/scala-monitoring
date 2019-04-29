80b0721a4651c7d1a76025023ddf99e96fbcae3d
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-04-29 16:58:45 +0100

    Extract Metrics Trait (in order that it can be abstracted elsewhere)

---
+++ monitoring/src/main/scala/uk/ac/wellcome/monitoring/Metrics.scala
@@ -0,0 +1,12 @@
+package uk.ac.wellcome.monitoring
+
+import scala.language.higherKinds
+
+trait Metrics[F[_], MetricUnit] {
+  def incrementCount(metricName: String): F[Unit]
+  def recordValue(
+                   metricName: String,
+                   value: Double,
+                   maybeUnit: Option[MetricUnit] = None
+                 ): F[Unit]
+}
