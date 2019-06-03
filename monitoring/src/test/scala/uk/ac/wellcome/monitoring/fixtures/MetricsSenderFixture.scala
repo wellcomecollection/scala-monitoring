59ea48f2b6c2a36fa127cbe967cf937e9a529f81
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-03 12:23:00 +0100

    Get Mockito out of the metrics fixture

--- monitoring/src/test/scala/uk/ac/wellcome/monitoring/fixtures/MetricsSenderFixture.scala
+++ monitoring/src/test/scala/uk/ac/wellcome/monitoring/fixtures/MetricsSenderFixture.scala
@@ -1,22 +1,13 @@
 package uk.ac.wellcome.monitoring.fixtures
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch
-import org.mockito.Matchers.anyString
-import org.mockito.Mockito.when
-import org.mockito.invocation.InvocationOnMock
-import org.scalatest.mockito.MockitoSugar
 import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.fixtures._
 import uk.ac.wellcome.monitoring.{MetricsConfig, MetricsSender}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
-trait MetricsSenderFixture
-    extends CloudWatch
-    with Akka
-    with MockitoSugar {
-
+trait MetricsSenderFixture extends CloudWatch with Akka {
   def withMetricsSender[R](
     cloudWatchClient: AmazonCloudWatch = cloudWatchClient)(
     testWith: TestWith[MetricsSender, R]): R =
@@ -33,16 +24,4 @@ trait MetricsSenderFixture
         testWith(metricsSender)
       }
     }
-
-  def withMockMetricsSender[R](testWith: TestWith[MetricsSender, R]): R = {
-    val metricsSender = mock[MetricsSender]
-
-    when(
-      metricsSender.incrementCount(anyString())
-    ).thenAnswer((invocation: InvocationOnMock) => {
-      Future.successful(())
-    })
-
-    testWith(metricsSender)
-  }
 }
